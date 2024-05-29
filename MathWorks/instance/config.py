574bad394a50356e1662e27f2cec738bba7a76c9
ridby <73934274+ridby@users.noreply.github.com>
2024-05-29 23:44:24 +0300

    Delete MathWorks directory

--- MathWorks/instance/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    UPLOAD_FOLDER = 'uploads/'
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
