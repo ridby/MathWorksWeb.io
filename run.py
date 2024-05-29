6c550a336133317fe72d00b0fc7d2e74b4a5173a
ridby <73934274+ridby@users.noreply.github.com>
2024-05-30 00:12:24 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app, db
+from flask_migrate import Migrate
 
 app = create_app()
+migrate = Migrate(app, db)
 
-with app.app_context():
-    db.create_all()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
