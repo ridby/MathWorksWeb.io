e40d0427206483dced463eb8291d8f6a2dfa5f55
ridby <73934274+ridby@users.noreply.github.com>
2024-05-30 00:12:02 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,23 +8,21 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'routes.login'  # Исправлено здесь
-login_manager.login_message_category = 'info'
-migrate = Migrate()
+login_manager.login_view = 'routes.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('instance.config.Config')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    migrate.init_app(app, db)
-    
-    from app import routes
-    app.register_blueprint(routes.bp)
-    
+
+    from app.routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
+
     with app.app_context():
         db.create_all()
-    
+
     return app
