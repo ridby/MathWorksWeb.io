39ad6a9348fbb12e538bd347fd48643c300f7770
ridby <73934274+ridby@users.noreply.github.com>
2024-05-30 03:29:00 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -43,7 +43,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('profile'))
+            return redirect(next_page) if next_page else redirect(url_for('routes.profile'))
         else:
             flash('Вход не выполнен. Пожалуйста, проверьте email и пароль.', 'danger')
     return render_template('login.html', title='Вход', form=form)
