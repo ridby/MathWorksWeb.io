50774d10342f721a0332bcb2a24ae9669a49d523
ridby <73934274+ridby@users.noreply.github.com>
2024-05-30 03:06:18 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-app = create_app()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-if __name__ == "__main__":
-    app.run()
+from app import routes
